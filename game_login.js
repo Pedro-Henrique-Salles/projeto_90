72788e66ced6409d679a66328b9c6b4a4652418b
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-05-28 12:23:35 -0400

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,7 +1,7 @@
 
 function addUser() {
-    player1Name = document.getElementById("player1NameInput").value;
-    player2Name = document.getElementById("player2NameInput").value;
+    player1Name = document.getElementById("player1Name").value;
+    player2Name = document.getElementById("player2Name").value;
   
       localStorage.setItem("player1Name", player1Name);
       localStorage.setItem("player2Name", player2Name);
