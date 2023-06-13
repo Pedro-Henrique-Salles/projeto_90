83875f5ba89e2a4ef82a076bba37c037b716fa91
Pedro-Henrique-Salles <110946918+Pedro-Henrique-Salles@users.noreply.github.com>
2023-06-13 17:50:39 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,11 +1,12 @@
-player1Name = localStorage.getItem("player1Name");
-	player2Name = localStorage.getItem("player2Name");
+var player1Name = localStorage.getItem("player1Name");
+var	player2Name = localStorage.getItem("player2Name");
 
-	player1Score = 0;
-	player2Score = 0;
+var	player1Score = 0;
+var	player2Score = 0;
 
 var  questionTurn="player1";
 var answerTurn="player2";
+var resposta_certa;
 
 document.getElementById("player1Name").innerHTML = player1Name + " : ";
 document.getElementById("player2Name").innerHTML = player2Name + " : ";
@@ -16,86 +17,28 @@ document.getElementById("player2Score").innerHTML = player2Score ;
 document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta - " + player1Name ;
 document.getElementById("playerAnswer").innerHTML = "Turno de Resposta - " + player2Name ;
 
-function check(){
-	getAnswer=document.getElementById("inputCheckBox").value;
-	answer=getAnswer.toLowerCase();
-	console.log("resposta em caixa baixa-"+answer);
-
-	if (answer==word){
-		if (answerTurn=="player1"){
-			player1=player1Score+1;
-			document.getElementById("player1Score").innerHTML=player1Score
-		}
-		else {
-			player2=player2Score+1;
-			document.getElementById("player2Score").innerHTML=player2Score;
-		}
-	}
-
-	if (questionTurn=="player1"){
-		questionTurn="player2";
-		document.getElementById("playerQuestion").innerHTML="turno de pergunta"+player2Name;
-	}
-	else {
-		questionTurn="player1";
-		document.getElementById("playerQuestion").innerHTML="turno de pergunta"+player1Name;
-	}
-
-	if (answerTurn=="player1"){
-		answerTurn="player2";
-		document.getElementById("playerAnswer").innerHTML="turno de resposta"+player2Name;
-	}
-
-	else {
-		answerTurn="player1";
-		document.getElementById("playerAnswer").innerHTML="turno de resposta"+player1Name;
-	}
-
-	document.getElementById("output")=""
-}
-
-
 
 function send() {
-	getWord = document.getElementById("word").value;
-	word = getWord.toLowerCase();
-	console.log("palavra em caixa baixa = " + word);
-
-    charAt1 = word.charAt(1);
-	console.log(charAt1);
-
-	lenghtDivide2 = Math.floor(word.length/2);
-	charAt2 = word.charAt(lenghtDivide2);
-	console.log(charAt2);
-
-    lenghtMinus1 = word.length - 1; 
-    charAt3 = word.charAt(lenghtMinus1); 
-	console.log(charAt3);
-
-    removeCharAt1 = word.replace(charAt1, "_");
-    removeCharAt2 = removeCharAt1.replace(charAt2, "_");
-    removeCharAt3 = removeCharAt2.replace(charAt3, "_");
-	console.log(removeCharAt3);
-
-    questionWord = "<h4 id='wordDisplay'> 4x6. "+removeCharAt3+"</h4>";
-    input_box = "<br>Resposta : <input type='text' id='inputCheckBox'>";
-    checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
-    row =  questionWord + input_box + checkButton ; 
+	var numero1=document.getElementById("numero1").value;
+	var numero2=document.getElementById("numero2").value;
+	resposta_certa=parseInt(numero1)*parseInt(numero2);
+    var question_number= "<h4 id='wordDisplay'>  "+numero1+" x "+numero2+"</h4>";
+    var input_box = "<br>Resposta : <input type='text' id='inputCheckBox'>";
+    var checkButton = "<br><br><button class='btn btn-info' onclick='check()'>Checar</button>";
+    var row =  question_number + input_box + checkButton ; 
     document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
+document.getElementById("numero1").value = "";
+document.getElementById("numero2").value = "";
 }
 
 
-questionTurn = "player1";
-answerTurn = "player2";
+
 
 
 function check()
 {
-	getAnswer = document.getElementById("inputCheckBox").value;
-	answer = getAnswer.toLowerCase();
-	console.log("resposta em caixa baixa - " + answer);
-	if(answer == word)	
+	var resposta_do_usuario = document.getElementById("inputCheckBox").value;
+	if(resposta_certa == resposta_do_usuario)	
 	{
 		if(answerTurn == "player1")
 		{
@@ -108,9 +51,9 @@ function check()
 		    document.getElementById("player2Score").innerHTML = player2Score;
 		}
 	}
-	if(questionTurn == "player1")
+	if(questionTurn == "player2")
 	{
-		questionTurn = "player2"
+		questionTurn = "player1"
 		document.getElementById("playerQuestion").innerHTML = "Turno de Pergunta: " + player2Name ;
 	}
 	else 
